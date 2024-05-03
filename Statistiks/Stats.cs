822b4e28a12b6ceb63b2b06e29d6a0a218d17e21
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-05-03 18:51:52 +0700

    inisialisasi Class Stats

---
+++ Statistiks/Stats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Stats
+{
+    private string filepath; // folder yang isinya csv
+    public Stats(string filepath)
+    {
+        this.filepath = filepath;
+    }
+    public string GetFilePath()
+    {
+        return filepath;
+    }
+
+    public static bool ExtCheck(string filepath)
+    {
+        string ext = Path.GetExtension(filepath);
+        return ext == ".csv";
+    }
+}
