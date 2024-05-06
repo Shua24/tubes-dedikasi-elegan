bf84bfd898177779b8a19e235b642e49ba85c695
shua24 <jo.daniel.simanjuntak@gmail.com>
2024-05-07 00:43:07 +0700

    Betulkan bug penggantian referensi; rapikan kode; tambahkan todo

--- Statistiks/Login-reg.cs
+++ Statistiks/Login-reg.cs
@@ -16,12 +16,12 @@
             this.Password = Password;
         }
 
-        public LoginState login()
+        public LoginState Login()
         {
             StateLogin loginState = new StateLogin();
             UserList user = new UserList();
 
-            Console.Write("Login:\n Username: ");
+            Console.Write("Login:\nUsername: ");
             string nameinput = Console.ReadLine();
             Console.Write("Password: ");
             string passwdInput = Console.ReadLine();
@@ -49,15 +49,15 @@
             yousef
         }
 
-        public string GetUsername(User user)
+        public static string GetUsername(User user)
         {
             String[] usernames =
-                {
-            "Rakha",
-            "Joshua",
-            "Aufa",
-            "Dzawin",
-            "Yousef",
+            {
+                "Rakha",
+                "Joshua",
+                "Aufa",
+                "Dzawin",
+                "Yousef",
             };
             int index = (int)user;
             if (index >= 0 && index < usernames.Length)
@@ -67,15 +67,15 @@
             return "Username Tidak Ditemukan";
         }
 
-        public string GetPassword(User user)
+        public static string GetPassword(User user)
         {
             String[] passwords =
-                {
-            "galih",
-            "daniel",
-            "taqiyya",
-            "nuha",
-            "gumilar",
+            {
+                "galih",
+                "daniel",
+                "taqiyya",
+                "nuha",
+                "gumilar",
             };
             int index = (int)user;
             if (index >= 0 && index < passwords.Length)
@@ -106,6 +106,7 @@
         Transition[] transitions =
         {
             new Transition(LoginState.BELUM_LOGIN, Trigger.LOGIN, LoginState.SUDAH_LOGIN),
+            // TODO: Tambah states untuk automata (Rakha)
         };
 
         public LoginState NextState(LoginState initLoginState, Trigger trigger)
--- Statistiks/Stats.cs
+++ Statistiks/Stats.cs
@@ -10,11 +10,11 @@ namespace CSVAnalytics
         private string filepath; // folder yang isinya csv
         private string csvFile; // file CSV referensi
         private ConfigurationReader conf;
-        public CSVTable(string csvfile)
+        public CSVTable(string csvFile)
         {
             this.conf = new ConfigurationReader();
             this.filepath = conf.config.directory;
-            this.csvFile = csvfile;
+            this.csvFile = csvFile;
         }
         
         public string GetFilePath()
@@ -57,8 +57,19 @@ namespace CSVAnalytics
                     Console.WriteLine("Contoh: file.csv");
                     Console.Write("Tabel anda: ");
                     string csvChange = Console.ReadLine();
-                    if (!File.Exists(csvChange) || !IsCSV(csvChange)) Console.WriteLine("File tidak ada atau file bukan CSV!");
-                    else csvFile = csvChange;
+                    if (!File.Exists(filepath+
+                        csvChange))
+                    {
+                        Console.WriteLine("File tidak tersedia!");
+                    }
+                    else if (!IsCSV(csvChange))
+                    {
+                        Console.WriteLine("File bukan CSV");
+                    }
+                    else
+                    {
+                        csvFile = csvChange;
+                    }
                     break;
                 case "n": Console.WriteLine("Tidak ganti referensi"); break;
                 default: Console.WriteLine("Pilihan tidak valid!"); break;
@@ -145,5 +156,4 @@ namespace CSVAnalytics
             table.Write(Format.Alternative);
         }
     }
-
 }
