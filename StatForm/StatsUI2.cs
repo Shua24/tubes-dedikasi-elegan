42a213015c25bab19540d5f5ebe0aafcbe5c3722
myousefg <myousefg@gmail.com>
2024-05-17 15:05:07 +0700

    menambahkan forms UI init

---
+++ StatForm/StatsUI2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatForm
+{
+    public partial class StatsUI2 : Form
+    {
+        public StatsUI2()
+        {
+            InitializeComponent();
+        }
+    }
+}
